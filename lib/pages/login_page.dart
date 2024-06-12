21f50a4fdc158bf9862d88d4de58e27d0b2255d5
jameschinedu313 <jameschinedu313@gmail.com>
2024-06-12 14:14:56 +0000

    first commit

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:1425041435.
+class LoginPage extends StatefulWidget{
+  @override
+  state<StatefulWidget>creatState(){
+    return null
+  }
+}  
